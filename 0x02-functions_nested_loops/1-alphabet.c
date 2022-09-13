1e9f9f28f45f1d6ed5e604ca525cc05520c0680f
Olufolahanmi <folahanmidavid@gmail.com>
2022-09-13 05:45:27 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	for (char alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+		putchar('\n');
+	}
+}
