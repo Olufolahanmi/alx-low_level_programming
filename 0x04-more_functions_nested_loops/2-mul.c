46b38fb4b12d4b70075b4eeaed3a2325f941966c
Olufolahanmi <folahanmidavid@gmail.com>
2022-09-15 12:40:01 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - FUNCTION THAT MULTIPLIES TWO INTERGERS
+ * @a: integer 1
+ * @b: integer 2
+ * Return: multiplied value
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
