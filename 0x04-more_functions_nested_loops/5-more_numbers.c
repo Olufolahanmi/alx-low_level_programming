d32d04554df950e5952a4fed0911e960429135f4
Olufolahanmi <folahanmidavid@gmail.com>
2022-09-15 13:08:30 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers -function description
+ */`
+void more_numbers(void)
+{
+	int i,j, a;
+	
+	for (i = 0; i<10; i++)
+	{
+		for (j = 0; j<= 14; j++)
+		{
+			if (j >= 10)
+			{
+				a = j / 10;
+				putchar(a + 48);
+			}
+			putchar(j%10 +48);
+		}
+		_putchar('\n');
+	}
+}
