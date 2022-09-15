28e05949b6e5edad2b81f48b9e686da41d2d01ec
Olufolahanmi <folahanmidavid@gmail.com>
2022-09-15 13:45:10 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+/**
+ * print_line - check for a digit
+ * @n : number of _ to be printed
+ * Return:void
+ */
+
+void print_line(int n)
+{
+
+	int i = 0;
+
+	while (i < n)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+
+}
